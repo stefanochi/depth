62c9645e1111e313a72634d48573b549c4b8ba88
Chiavazza, Stefano <stefano.chiavazza@intel.com>
2022-07-02 14:18:09 +0200

    initial structure

---
+++ gtd2d/data_loader/data_loader.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class DataLoader():
+    """Generic data loader class
+    Contains function to load and prepare the data to be used by the runner.
+    The data preparation will be different depending on the runner that will be used"""
+    def __init__(self):
+        return
---
+++ gtd2d/plotter/plotter.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class Plotter:
+    """Class the handles all the processing of the output data
+    to produce plots and data. The putput format of the runners will be different,
+    but the data and information return should be the same and comparable."""
+    def __init__(self):
+        return
