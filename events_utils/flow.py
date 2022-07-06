fix flow field computation error

events_utils/flow.py
@@ -7,8 +7,8 @@ def get_translational_flow(t, f, C, shape):
     for x in range(u_flow.shape[1]):
         for y in range(u_flow.shape[0]):
             # shift coordinates to be centered
-            xi = x - C[0]
-            yi = np.abs(y - u_flow.shape[0]) - C[1]
+            xi = x - C[1]
+            yi = np.abs(y - u_flow.shape[0] +1) - C[0]
 
             # compute image flow
             m = np.array([

gtd2d/runner/flow_utils.py
@@ -9,7 +9,7 @@ def get_translational_flow(t, f, C, shape):
         for y in range(u_flow.shape[0]):
             # shift coordinates to be centered
             xi = x - C[0]
-            yi = np.abs(y - u_flow.shape[0]) - C[1]
+            yi = np.abs(y - u_flow.shape[0] +1) - C[1]
 
             # compute image flow
             m = np.array([
@@ -31,7 +31,7 @@ def get_angular_flow(w, f, C, shape):
         for y in range(u_flow.shape[0]):
             # center
             xi = x - C[0]
-            yi = np.abs(y - u_flow.shape[0]) - C[1]
+            yi = np.abs(y - u_flow.shape[0] +1) - C[1]
 
             m = np.array([
                 [(xi * yi) / f, -(xi ** 2) / f - f, yi],