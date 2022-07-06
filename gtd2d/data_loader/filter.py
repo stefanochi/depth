433ea96282f24e9743a84be0b7c51526090ebcf3
Chiavazza, Stefano <stefano.chiavazza@intel.com>
2022-07-06 17:27:51 +0200

    added convolution downsampling

--- gtd2d/data_loader/filter.py
+++ gtd2d/data_loader/filter.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def filter_refract(events, ref_time):
     time_dict = {}
     result = []
@@ -20,8 +21,32 @@ def filter_refract(events, ref_time):
 
     return np.array(result)
 
+
 def filter_time(events, t_start, t_end):
-    id_start = np.searchsorted(events[:,0], t_start)
-    id_end = np.searchsorted(events[:,0], t_end)
+    id_start = np.searchsorted(events[:, 0], t_start)
+    id_end = np.searchsorted(events[:, 0], t_end)
 
     return events[id_start:id_end, :]
+
+
+def filter_conv(events, shape, factor=5, thresh=1.0 / 2.0):
+    conv_shape = (factor, factor)
+    conv_stride = (factor, factor)
+    result_shape = (int(shape[0] / conv_stride[0]), int(shape[1] / conv_stride[1]))
+    conv_acc = np.zeros(result_shape)
+    event_conv = []
+    for e in events:
+        # if e[3] == 0:
+        #     continue
+
+        x = int(e[1] / conv_stride[0])
+        y = int(e[2] / conv_stride[1])
+
+        conv_acc[y, x] += 1 if e[3] == 1 else -1
+
+        if np.abs(conv_acc[y, x]) >= conv_shape[0] * conv_shape[1] * thresh:
+            event_conv.append([e[0], x, y, e[3]])
+            conv_acc[y, x] = 0
+
+    event_conv = np.array(event_conv)
+    return event_conv, result_shape
